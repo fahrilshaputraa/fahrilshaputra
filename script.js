51453417e792ae0fff39107f156cc4a6b3af6eb6
fahrilshaputraa <fahrilshaputra@gmail.com>
2022-11-05 06:33:29 +0700

    update

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ window.addEventListener("scroll", () => {
   const windowPosition = window.scrollY > 200;
   navBar.classList.toggle("backActive", windowPosition);
 });
+
+$(document).ready(function () {
+  $(".nav-link").on("click", function () {
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+  });
+});
