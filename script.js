c9e11114892784c54bcb36589e977ddab898be7d
fahrilshaputraa <fahrilshaputra@gmail.com>
2022-11-05 07:00:27 +0700

    new update

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ window.addEventListener("scroll", () => {
   navBar.classList.toggle("backActive", windowPosition);
 });
 
-$(document).ready(function () {
-  $(".nav-link").on("click", function () {
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-  });
-});
+const li = document.querySelectorAll(".nav-link");
+const sec = document.querySelectorAll("section");
+
+function activeMenu() {
+  let len = sec.length;
+  while (--len && window.scrollY + 97 < sec[len].offsetTop) {}
+  li.forEach((ltx) => ltx.classList.remove("active"));
+  li[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+// $(document).ready(function () {
+//   $(".nav-link").on("click", function () {
+//     $(this).siblings().removeClass("active");
+//     $(this).addClass("active");
+//   });
+// });
